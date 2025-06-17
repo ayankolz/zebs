cc409d040462859464de039616e263fc3e9cf6b7
Ayankolz <ayankoyadavid6@gmail.com>
2025-06-17 18:18:54 +0100

    fix: updated MySQL connection settings and .env configuration

--- server/routes/userroute.js
+++ server/routes/userroute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { db } from './db.js';
+import { db } from '../db.js';
 import nodemailer from 'nodemailer';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
